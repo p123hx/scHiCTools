5b89c07803127f25e6202a6956297a9438ac8b0c
hongxi <hongxi@umich.edu>
2020-12-15 09:32:55 -0500

    init

---
+++ cmake-build-debug/CMakeFiles/scHiCtools.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/scHiCtools.dir/main.cpp.o"
+  "scHiCtools"
+  "scHiCtools.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/scHiCtools.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
