c5950f166614c358cdf3c8ba8a88c5949bde4b27
hongxi <hongxi@umich.edu>
2021-01-19 12:40:12 -0500

    ini

--- test/test_reproducibility.cpp
+++ test/test_reproducibility.cpp
@@ -2,25 +2,28 @@
 // Created by Bean Juice on 06/01/2021.
 //
 
-//#include "xtensor/xio.hpp"
-//#include "../load/ContactMaps.h"
-//#include "../embedding/reproducibility.h"
-//
-//using namespace std;
-//void innerP(vector<xt::xarray<double>> all_strata){    xt::xarray<double> pair_dis =
-//pairwise_distance(all_strata, "inner_product");
-//    cout << "pairwise dis: " << pair_dis << endl;}
-//int main() {
-//    vector<string> fileLst{"../test/data/cell_03", "../test/data/cell_01",
-//                           "../test/data/cell_02"};
-//    vector<string> operation{"convolution"};
-//    scHiCs y = scHiCs(fileLst, "mm9", 500000, 3, 4000000, true, "except Y",
-//                      "shortest_score",
-//                      10, true,
-//                      operation);
-//    vector<xt::xarray<double>> all_strata = y.get_strata()["chr8"];
-//    innerP(all_strata);
-//    xt::xarray<double> pair_dis = pairwise_distance(all_strata, "hicrep");
+#include "xtensor/xio.hpp"
+#include "../load/ContactMaps.h"
+#include "../embedding/reproducibility.h"
 
+using namespace std;
+double innerP(vector<xt::xarray<double>> all_strata){    xt::xarray<double> pair_dis;
+double time1; tie(pair_dis,time1)=
+pairwise_distance(all_strata, "inner_product");
+    cout << "pairwise dis: " << pair_dis << endl;
+return time1;}
+int main() {
+    vector<string> fileLst{"../test/data/cell_03", "../test/data/cell_01",
+                           "../test/data/cell_02"};
+    vector<string> operation{"convolution"};
+    scHiCs y = scHiCs(fileLst, "mm9", 500000, 3, 4000000, true, "except Y",
+                      "shortest_score",
+                      10, true,
+                      operation);
+    vector<xt::xarray<double>> all_strata = y.get_strata()["chr8"];
+    double tsum=innerP(all_strata);
+    //xt::xarray<double> pair_dis;double time1; tie(pair_dis,time1)= pairwise_distance
+      //      (all_strata, "hicrep");
+cout<<"time: "<<tsum;
 }
 
